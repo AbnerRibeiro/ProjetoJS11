55087d274029550b298c54ff858f4ac76deb7956
Abner Ribeiro <abnergoulart@yahoo.com.br>
2019-07-09 21:47:39 -0300

    acrescentando parametros

--- js/main.js
+++ js/main.js
@@ -1,4 +1,25 @@
 $(document).ready(function(){
+	const button = $('#button');
+
+	button.click(function() {
+		let nomePais = $('#nomepais').val();
+		const api = `https://restcountries.eu/rest/v2/name/${nomePais}`;
+		
+		$.getJSON(api, function(retorno_api){
+			console.log(retorno_api);
+			let nomeNativo = retorno_api[0].nativeName;
+			let bandeira = retorno_api[0].flag; 
+			let populacao = retorno_api[0].population;
+			$('#resposta').append(`
+			
+				<p>População: ${populacao}</p>
+				<p>Nome: ${nomeNativo}<p>
+				<img src="${bandeira}" class="bandeiras">
+
+			`);
+
+		});
+	}); 
 
 
 });
